0dc9a39fa23bab1b86a25c334345c338b537bb27
Tim Booth <tim.booth@ed.ac.uk>
2025-02-25 15:50:36 +0000

    Fix a test failure.

--- .test/ep07.test.sh
+++ .test/ep07.test.sh
@@ -6,10 +6,10 @@ cd snakemake_data/yeast
 # This Snakefile assumes renames are applied
 ( cd reads ; rename -v -s ref ref_ ref?_?.fq )
 
-snakemake -j1 -p trimmed_counts_concatenated.txt \
+snakemake -j1 -p all_read1_removed.txt \
                  trimmed.etoh60_1_1_fastqc.zip \
                  reads.etoh60_1_1_fastqc.zip
 
-test -s trimmed_counts_concatenated.txt
+test -s all_read1_removed.txt
 test -s trimmed.etoh60_1_1_fastqc.html
 test -s reads.etoh60_1_1_fastqc.html
